c015fa61832a9a945e8aa456e250d84563d76b90
Okste1234 <okste1234@gmail.com>
2022-09-28 00:21:01 +0100

    Changing Router for deployment

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -14,7 +14,7 @@ const HeroBanner = () => (
             Check out the most effective exercises personalized to you
         </Typography>
         <Stack>
-            <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+            <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px', m: "4" }}>Explore Exercises</a>
         </Stack>
         <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
             Exercise
